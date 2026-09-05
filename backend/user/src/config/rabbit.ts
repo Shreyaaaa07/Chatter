1d689a3a7c34f2ae43651b4f11ee5ce7e9b5b342
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-09-05 21:43:56 +0530

    added packages

--- backend/user/src/config/rabbit.ts
+++ backend/user/src/config/rabbit.ts
@@ -1,22 +1,21 @@
-import * as amqplib from 'amqplib'; // Changed to standard namespace import
+import amqplib from 'amqplib'
 
 let channel: amqplib.Channel;
 
-export const connectRabbitMQ = async () => {
-  try {
-    const connection = await amqplib.connect({ // Fixed typo: 'amqp.connect' -> 'amqplib.connect'
-      protocol: "amqp",
-      hostname: process.env.Rabbitmq_Host,
-      port: 5672,
-      username: process.env.RabbitMQ_Username,
-      password: process.env.RabbitMQ_Password,
-    });
-    
-    channel = await connection.createChannel();
-    console.log("Connected to RabbitMQ");
-    
-  } catch (error) {
-    console.error("Failed to connect:", error);
-    process.exit(1);
-  }
-};
+export const  connectRabbitMQ = async() => {
+    try{
+        const connection =await amqp.connect({
+          protocol: "amqp",
+          hostname: process.env.Rabbitmq_Host,
+          port: 5672,
+          username: process.env.RabbitMQ,
+          password: process.env.RabbitMQ_Password,  
+        });
+        channel = await connection.createChannel();
+        console.log("Connected to RabbitMQ");
+
+    } catch (error) {
+        console.error('Failed to connect:', error);
+        process.exit(1);
+    }
+}
