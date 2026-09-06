1938de2a988946788c2d552702dbacbc12864b33
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-09-06 16:35:02 +0530

    added trycatch code to get otp

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -1,7 +1,21 @@
-import TryCatch  from "../config/TryCatch.js";
+import TryCatch from "../config/TryCatch.js";
+import { redisClient } from "../index.js";
 
- export const LoginUser = TryCatch(async(req,res)=>{
-    const {email}=req.body
+export const LoginUser = TryCatch(async (req, res) => {
+    const { email } = req.body;
+    const rateLimitKey = `email:${email}`;
+    
+    const rateLimit = await redisClient.get(rateLimitKey);
+    
+    if (rateLimit) {
+        return res.status(429).json({
+            message: "Too many requests. Please wait before requesting new OTP.",
+            error: "Too many requests"
+        });
+    }
 
-    const rateLimitKey = `otp:ratelimet;${email}`;
- } )
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    
+    // Your next logic goes here (e.g., saving OTP to redis or sending email)
+
+});
