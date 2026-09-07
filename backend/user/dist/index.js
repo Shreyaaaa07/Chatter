a7dc3592443e483c3552dd5d734bf132de0a0fac
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-09-07 17:39:58 +0530

    added new packages

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -16,7 +16,7 @@ export const redisClient = createClient({
 });
 redisClient.connect().then(() => console.log("Connected to Redis")).catch(console.error);
 const app = express();
-app.use("api/v1", userRoutes);
+app.use("/api/v1", userRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -23,7 +23,7 @@ redisClient.connect().then(()=>console.log("Connected to Redis")) .catch(console
 
 const app = express()
 
-app.use("api/v1", userRoutes) 
+app.use("/api/v1", userRoutes) 
 
 const port = process.env.PORT
 
